63103b485de57d77072dbf78c8c3b86206fd1be5
flavio-secondini <flavio.secondini@gmail.com>
2021-03-12 09:28:43 +0100

    definita var active all'interno della funzioni

--- main.js
+++ main.js
@@ -6,22 +6,26 @@ var last = $("img.last")
 
 // se clicco sulla freccia destra muovo la classe .active da un fratello all'altro dall'alto verso il basso. Arrivato all'ultimo fratello, la sposto di nuovo sul primo
 rightArrow.click(function() {
+  // definisco la variabile active all'interno della funzione perchè dovrà essere cercata ad ogni click
+  var active = $("img.active")
   if (last.hasClass("active")) {
     last.removeClass("active")
     first.addClass("active")
   } else {
-    $("img.active").removeClass("active").next().addClass("active")
+    active.removeClass("active").next().addClass("active")
   }
 })
 
 // stessi comandi della freccia destra, ma al contrario. Se il primo fratello ha classe .active, la sposto sull'ultimo, altrimenti la sposto dal basso verso l'alto
 leftArrow.click(function() {
+
+  var active = $("img.active")
   if (first.hasClass("active")) {
     first.removeClass("active")
     last.addClass("active")
   }
   else {
-    $("img.active").removeClass("active").prev().addClass("active")
+    active.removeClass("active").prev().addClass("active")
   }
 })
 
@@ -29,22 +33,26 @@ leftArrow.click(function() {
 $(document.documentElement).keydown(function (e){
   // freccia destra della tastiera
   if (e.keyCode == 39) {
+
+    var active = $("img.active")
     if (last.hasClass("active")) {
       last.removeClass("active")
       first.addClass("active")
     } else {
-      $("img.active").removeClass("active").next().addClass("active")
+      active.removeClass("active").next().addClass("active")
     }
   }
 
   // freccia sinistra della tastiera
   if (e.keyCode == 37) {
+
+    var active = $("img.active")
     if (first.hasClass("active")) {
       first.removeClass("active")
       last.addClass("active")
     }
     else {
-      $("img.active").removeClass("active").prev().addClass("active")
+      active.removeClass("active").prev().addClass("active")
     }
   }
 
